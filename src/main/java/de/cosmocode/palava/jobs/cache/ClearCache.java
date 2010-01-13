0f642eb7ce07726fab81a46936c9da87e0e3cda6
schoenborn <schoenborn@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-01-13 05:16:34 +0000

    started to refactor palava core

---
+++ src/main/java/de/cosmocode/palava/jobs/cache/ClearCache.java
@@ -0,0 +1,59 @@
+/**
+ * palava - a java-php-bridge
+ * Copyright (C) 2007  CosmoCode GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package de.cosmocode.palava.jobs.cache;
+
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.cosmocode.palava.Job;
+import de.cosmocode.palava.Server;
+import de.cosmocode.palava.core.protocol.JSONContent;
+import de.cosmocode.palava.core.protocol.Request;
+import de.cosmocode.palava.core.protocol.Response;
+import de.cosmocode.palava.core.session.HttpSession;
+import de.cosmocode.palava.services.cache.CacheService;
+
+/**
+ * 
+ *
+ * @author Willi Schoenborn
+ */
+public final class ClearCache implements Job {
+
+    private static final Logger log = LoggerFactory.getLogger(ClearCache.class);
+
+    
+    private CacheService cacheService;
+    
+    @Override
+    public void process(Request request, Response response, HttpSession session, Server server, Map<String, Object> caddy)
+        throws Exception {
+
+        log.info("Clearing cache");
+        cacheService.clear();
+        log.info("Cache cleared");
+        
+        response.setContent(JSONContent.EMPTY);
+        
+    }
+
+}
---
+++ src/main/java/de/cosmocode/palava/jobs/cache/RefreshCache.java
@@ -0,0 +1,29 @@
+/**
+ * palava - a java-php-bridge
+ * Copyright (C) 2007  CosmoCode GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package de.cosmocode.palava.jobs.cache;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RefreshCache {
+
+    private static final Logger log = LoggerFactory.getLogger(RefreshCache.class);
+
+}
