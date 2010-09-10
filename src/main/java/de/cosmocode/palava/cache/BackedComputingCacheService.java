54b1b6969b1bfef326ba55969630f9a148435f84
schoenborn <schoenborn@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-09-10 17:26:34 +0000

    replaced list with queue

--- src/main/java/de/cosmocode/palava/cache/BackedComputingCacheService.java
+++ src/main/java/de/cosmocode/palava/cache/BackedComputingCacheService.java
@@ -17,13 +17,14 @@
 package de.cosmocode.palava.cache;
 
 import java.io.Serializable;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CancellationException;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
@@ -51,19 +52,18 @@ final class BackedComputingCacheService implements ComputingCacheService {
     
     private final CacheService service;
 
-    private final ConcurrentMap<Serializable, List<ValueFuture<Object>>> computations;
+    private final ConcurrentMap<Serializable, Queue<ValueFuture<Object>>> computations;
     
     @Inject
     public BackedComputingCacheService(CacheService service) {
         this.service = Preconditions.checkNotNull(service, "Service");
         
-        // soft values should remove obsolete lists
-        final MapMaker maker = new MapMaker().softValues();
-        this.computations = maker.makeComputingMap(new Function<Serializable, List<ValueFuture<Object>>>() {
+        final MapMaker maker = new MapMaker();
+        this.computations = maker.makeComputingMap(new Function<Serializable, Queue<ValueFuture<Object>>>() {
             
             @Override
-            public List<ValueFuture<Object>> apply(Serializable from) {
-                return new CopyOnWriteArrayList<ValueFuture<Object>>();
+            public Queue<ValueFuture<Object>> apply(Serializable from) {
+                return new ConcurrentLinkedQueue<ValueFuture<Object>>();
             }
             
         });
@@ -124,46 +124,46 @@ final class BackedComputingCacheService implements ComputingCacheService {
         Preconditions.checkArgument(maxAge >= 0, "Max age must not be negative");
         Preconditions.checkNotNull(maxAgeUnit, "MaxAgeUnit");
 
-        final List<ValueFuture<Object>> futures = computations.get(key);
+        final Collection<ValueFuture<Object>> futures = computations.get(key);
         final ValueFuture<Object> future = ValueFuture.create();
         
         futures.add(future);
         
-        if (future.isCancelled()) {
-            LOG.trace("{} has been cancelled", future);
-        } else if (future.isDone()) {
-            LOG.trace("{} has already finished", future);
-        } else {
-            try {
-                final V value = callable.call();
-                if (future.isDone()) {
-                    LOG.debug("Another computation was faster and already computed a value for {}", key);
-                } else {
-                    LOG.trace("Computed value '{}' for key '{}'", value, key);
-                    future.set(value);
-                    for (ValueFuture<Object> other : futures) {
-                        if (other == future) {
-                            // every computation after this is newer
-                            break;
-                        } else if (other.isDone()) {
-                            LOG.trace("Skipping finished computation: {}", other);
-                            continue;
-                        } else {
-                            // make older and running computations use my computed value
-                            LOG.trace("Setting faster computed value '{}' on {}", value, other);
-                            other.set(value);
-                        }
+        try {
+            final V value = callable.call();
+            if (future.isCancelled()) {
+                LOG.trace("{} has been cancelled", future);
+            } else if (future.isDone()) {
+                LOG.debug("Another computation was faster and already computed a value for {}", key);
+            } else {
+                LOG.trace("Computed value '{}' for key '{}'", value, key);
+                future.set(value);
+                for (ValueFuture<Object> other : futures) {
+                    if (other == future) {
+                        // every computation after this is newer
+                        break;
+                    } else if (other.isDone()) {
+                        LOG.trace("Skipping finished computation: {}", other);
+                        continue;
+                    } else {
+                        // make older and running computations use my computed value
+                        LOG.trace("Setting faster computed value '{}' on {}", value, other);
+                        other.set(value);
                     }
-                    LOG.trace("Storing '{}' => '{}' in underlying store", key, value);
-                    service.store(key, value, maxAge, maxAgeUnit);
                 }
-            /* CHECKSTYLE:OFF */
-            } catch (Exception e) {
-            /* CHECKSTYLE:ON */
-                LOG.warn("Exception during {}.call()", callable);
-                future.setException(e);
-            } finally {
-                futures.remove(future);
+                LOG.trace("Storing '{}' => '{}' in underlying store", key, value);
+                service.store(key, value, maxAge, maxAgeUnit);
+            }
+        /* CHECKSTYLE:OFF */
+        } catch (Exception e) {
+        /* CHECKSTYLE:ON */
+            LOG.warn("Exception during {}.call()", callable);
+            future.setException(e);
+        } finally {
+            futures.remove(future);
+            // TODO make threadsafe
+            if (futures.isEmpty()) {
+                computations.remove(key);
             }
         }
         
@@ -179,19 +179,19 @@ final class BackedComputingCacheService implements ComputingCacheService {
     @Override
     public <T> T read(Serializable key) {
         Preconditions.checkNotNull(key, "Key");
-        final List<ValueFuture<Object>> futures = computations.get(key);
+        final Collection<ValueFuture<Object>> futures = computations.get(key);
         
         if (futures.isEmpty()) {
             LOG.trace("Reading pre-computed value for {} from underlying cache", key);
             // no running computation, the easy part
             return service.<T>read(key);
         } else {
-            final ListIterator<ValueFuture<Object>> iterator = futures.listIterator(futures.size());
+            final Iterator<ValueFuture<Object>> iterator = futures.iterator();
             try {
-                final ValueFuture<Object> last = iterator.previous();
-                LOG.trace("Waiting for {} to compute value for key {}", last, key);
+                final ValueFuture<Object> future = iterator.next();
+                LOG.trace("Waiting for {} to compute value for key {}", future, key);
                 @SuppressWarnings("unchecked")
-                final T t = (T) last.get();
+                final T t = (T) future.get();
                 return t;
             } catch (CancellationException e) {
                 LOG.debug("Computation for {} has been cancelled during read", key);
@@ -208,7 +208,7 @@ final class BackedComputingCacheService implements ComputingCacheService {
     @Override
     public <T> T remove(Serializable key) {
         Preconditions.checkNotNull(key, "Key");
-        final List<ValueFuture<Object>> futures = computations.get(key);
+        final Collection<ValueFuture<Object>> futures = computations.get(key);
         
         if (futures.isEmpty()) {
             LOG.trace("Removing {} from underlying cache", key);
@@ -221,6 +221,11 @@ final class BackedComputingCacheService implements ComputingCacheService {
                 future.set(null);
                 // futures in concurrent, so this should work
                 futures.remove(future);
+                computations.remove(null, null);
+                // TODO make threadsafe
+                if (futures.isEmpty()) {
+                    computations.remove(key);
+                }
             }
             // make sure the underlying cache removes any pre-computed value
             return service.<T>remove(key);
