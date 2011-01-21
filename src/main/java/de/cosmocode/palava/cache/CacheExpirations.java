94ff7586d690dc17eb98ab8067d1d403cfde2d68
Willi Schönborn <schoenborn@cosmocode.de>
2011-01-21 18:30:38 +0100

    made CacheExpirations static class

--- src/main/java/de/cosmocode/palava/cache/CacheExpirations.java
+++ src/main/java/de/cosmocode/palava/cache/CacheExpirations.java
@@ -18,70 +18,19 @@ package de.cosmocode.palava.cache;
 
 import java.util.concurrent.TimeUnit;
 
-import com.google.common.base.Preconditions;
-
 /**
  * Reusable {@link CacheExpiration}s.
  *
  * @since 3.0
  * @author Willi Schoenborn
  */
-public enum CacheExpirations implements CacheExpiration {
+public final class CacheExpirations {
 
-    ETERNAL(0L, TimeUnit.MINUTES, 0L, TimeUnit.MINUTES);
+    public static final CacheExpiration ETERNAL = 
+        new DefaultCacheExpiration(0L, TimeUnit.MINUTES, 0L, TimeUnit.MINUTES);
     
-    private final long lifeTime;
-    private final TimeUnit lifeTimeUnit;
-    private final long idleTime;
-    private final TimeUnit idleTimeUnit;
-    
-    private CacheExpirations(long lifeTime, TimeUnit lifeTimeUnit, long idleTime, TimeUnit idleTimeUnit) {
-        Preconditions.checkArgument(lifeTime >= 0, "LifeTime must not be negative");
-        Preconditions.checkNotNull(lifeTimeUnit, "LifeTimeUnit");
-        Preconditions.checkArgument(idleTime >= 0, "IdleTime must not be negative");
-        Preconditions.checkNotNull(idleTimeUnit, "IdleTimeUnit");
-
-        this.lifeTime = lifeTime;
-        this.lifeTimeUnit = lifeTimeUnit;
-        this.idleTime = idleTime;
-        this.idleTimeUnit = idleTimeUnit;
-    }
-
-    @Override
-    public boolean isEternal() {
-        return lifeTime == 0L && idleTime == 0L;
-    }
-
-    @Override
-    public long getLifeTime() {
-        return lifeTime;
-    }
-
-    @Override
-    public TimeUnit getLifeTimeUnit() {
-        return lifeTimeUnit;
-    }
-
-    @Override
-    public long getLifeTimeIn(final TimeUnit unit) {
-        Preconditions.checkNotNull(unit, "Unit");
-        return unit.convert(lifeTime, lifeTimeUnit);
-    }
-
-    @Override
-    public long getIdleTime() {
-        return idleTime;
-    }
-
-    @Override
-    public TimeUnit getIdleTimeUnit() {
-        return idleTimeUnit;
-    }
-
-    @Override
-    public long getIdleTimeIn(final TimeUnit unit) {
-        Preconditions.checkNotNull(unit, "Unit");
-        return unit.convert(idleTime, idleTimeUnit);
+    private CacheExpirations() {
+        
     }
     
 }
